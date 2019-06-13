46da70594d6516fc11d7b59e1340dcdadad31b1d
Krys <krysngabi@gmail.com>
2019-06-13 14:13:10 -0500

    closure

--- closure/account.js
+++ closure/account.js
@@ -1,36 +1,43 @@
-var accountInfoList=[];
-window.onload = chargement;
-function chargement() {
-	document.getElementById("create").onclick = createAccount;
-
+(function () {
+    "use strict"
+window.onload=function(){
+	document.getElementById("create").onclick=createaccount;
 }
-function Account(accname, balance) {
-	this.accname = accname,
-	this.balance = balance,
-	this.fulldetails = function() {
-		return "\n Account Name : "+this.accname+" Balance "+this.depvalue;
+
+var accountarr=[];
+var i=0;
+var createaccount=(function clickacc(){
+	
+	var bankaccount=function(){
+		
+		var acctType=document.getElementById("account").value;
+		var deposit=document.getElementById("deposit").value;
+		var getaccttype = function () {
+			return acctType
+			};
+          var  getdeposit = function () {
+			  return deposit };
+			
+			return {
+				getaccttype:getaccttype,
+				getdeposit:getdeposit
+			}
+		} 
+		return function(){
+			accountarr.push(bankaccount());
+			displayAccount();
+		}
+		
+		
+	})();
+	var displayAccount=function(){	
+		var textacc="";
+		for (let acct of accountarr) {
+            textacc += `Account: ${acct.getaccttype()}  Balance: ${acct.getdeposit()} \n`;
+        }
+		  document.getElementById("mtext").value = textacc;
 	}
-}
-function createAccount(){
-    var accnmae = document.getElementById("account").value;
-    var depvalue = document.getElementById("deposit").value;
-
-    var newAccount = new Account(accnmae, depvalue);
+		
    
-    var taille=accountInfoList.push(accnmae);
-    addToArea();
-   
-   // var mtexta = document.getElementById("mtext");
-   // mtexta.value +="\n Account Name : "+accnmae+" Balance "+depvalue;
-
-}
-function addToArea(){
-  
-    //const values = Object.values(accountInfoList)
-    alert("ca donne ");
-    for (var i = 0; i < accountInfoList.length; i++) {
-        alert(accountInfoList[i]);
-        //Do something
-    }
-
-}
+})();
+	
